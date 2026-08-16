dfb7f3f1f7e163924f45a4541a7dee2e976c664d
TCARTS-Campus-Shield-2026 <gowtham8a2@gmail.com>
2026-08-16 12:00:06 +0530

    Update Studentlayout.tsx

--- CampusShield/src/layouts/Studentlayout.tsx
+++ CampusShield/src/layouts/Studentlayout.tsx
@@ -1,1 +1,16 @@
-1
+import { Outlet } from "react-router-dom";
+import Header from "../components/common/Header";
+import BottomNavigation from "../components/common/BottomNavigation";
+
+function StudentLayout() {
+  return (
+    <div className="min-h-screen bg-blue-50/40">
+      <Header />
+      <Outlet />
+      <BottomNavigation />
+    </div>
+  );
+}
+
+export default StudentLayout;
+
